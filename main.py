7459fed059853d4d89b161bba60d277f91c23ca3
Konovalov Vyacheslav <slava2002.konovalov@gmail.com>
2024-07-17 12:46:15 +0300

    modified requirements and main

--- main.py
+++ main.py
@@ -8,9 +8,5 @@ async def hello(name: str = Query(None, alias="name"),
                 message: str = Query(None, alias="message")):
     name = "Someone" if name is None else name
     message = "hmm.. are you here?" if message is None else message
-    return f"Hello {name} {message}"
+    return f"Hello {name}! {message}"
 
-
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
